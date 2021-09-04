2efd00ccc280157730b59b3da2ce81912bef46dc
suhridh <ssg.2711@gmail.com>
2021-09-04 16:28:43 +0530

    added UC2

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,5 +1,11 @@
 public class EmpWageBuilderUC1 {
     public static void main (String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        int IS_FULL_TIME = 1;
+        double EMP_CHECK = Math.floor(Math.random() * 10) % 2;
+        if (EMP_CHECK == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
