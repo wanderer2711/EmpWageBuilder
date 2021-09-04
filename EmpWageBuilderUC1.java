681ae60b435c3b1ce8b91e314f6ce8674291b0fa
suhridh <ssg.2711@gmail.com>
2021-09-04 17:21:45 +0530

    added UC5

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,16 +1,22 @@
 public class EmpWageBuilderUC1 {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int WAGE_PER_HOUR = 20;
     public static void main (String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int WAGE_PER_HOUR = 20;
         int empWage = 0;
         int empHr = 0;
-        double EMP_CHECK = Math.floor(Math.random() * 10) % 3;
-        if (EMP_CHECK == IS_FULL_TIME)
-            empHr = 8;
-        else if(EMP_CHECK == IS_PART_TIME)
-            empHr = 4;
+        int EMP_CHECK = (int) Math.floor(Math.random() * 10) % 3;
+        switch (EMP_CHECK) {
+            case IS_FULL_TIME:
+                empHr = 8;
+                break;
+            case IS_PART_TIME:
+                empHr = 4;
+                break;
+            default:
+                empHr = 0;
+        }
         empWage = empHr * WAGE_PER_HOUR;
         System.out.println("Employee Wage: " + empWage);
     }
